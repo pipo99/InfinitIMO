af7bd3f6d11ac159e3a91b7285e905b797541104
pipo99 <pipo99@gmail.com>
2023-08-09 18:59:07 +0100

    Property Service Documentation
    - Generate API documentation using open source tool redocly

--- PropertyService/Program.cs
+++ PropertyService/Program.cs
@@ -1,11 +1,29 @@
 using Microsoft.EntityFrameworkCore;
 using PropertyService.Data;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Property Service",
+        Description = "API InfinitIMO - Property Service",
+        Version = "v1"
+    });
+
+    var filename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var filepath = Path.Combine(AppContext.BaseDirectory, filename);
+    c.IncludeXmlComments(filepath);
+});
+
+
+
 builder.Services.AddSwaggerGen();
 
 // Configure DbContext for Property
@@ -26,6 +44,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
